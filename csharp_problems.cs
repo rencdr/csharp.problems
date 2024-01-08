931615169a0775b032de77a76f64be249a219571
rencdr <demirerencan@outlook.com>
2024-01-08 22:14:36 +0300

    problem 2 added

--- csharp_problems.cs
+++ csharp_problems.cs
@@ -1,4 +1,4 @@
-/*Two Sum -Given an array of numbers, find the two numbers such that they add up to a specific target.
+/*1.Two Sum -Given an array of numbers, find the two numbers such that they add up to a specific target.
 public int[] TwoSum(int[] nums, int target) {
     Dictionary<int, int> numIndices = new Dictionary<int, int>();
     
@@ -11,5 +11,20 @@ public int[] TwoSum(int[] nums, int target) {
     }
     
     return new int[] { -1, -1 }; // Eğer çözüm bulunamazsa
+}
+*/
+/*2.Reverse Integer-Reverse an integer. For example, reverse 123 to get 321.
+public int Reverse(int x) {
+    int sign = x >= 0 ? 1 : -1;
+    x = Math.Abs(x);
+    
+    long reversedX = 0;
+    while (x > 0) {
+        reversedX = reversedX * 10 + x % 10;
+        x /= 10;
+    }
+    
+    reversedX *= sign;
+    return (reversedX >= int.MinValue && reversedX <= int.MaxValue) ? (int)reversedX : 0;
 }
 */
