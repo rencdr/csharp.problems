f561eb7bc1ef63d8609edc7b2e6b66ceffac1591
rencdr <demirerencan@outlook.com>
2024-01-16 18:31:46 +0300

    problem 9 added

--- csharp_problems.cs
+++ csharp_problems.cs
@@ -130,5 +130,41 @@ public class Kata
         return result;
     }
 }
+*/
+/*
+9.Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+using System;
+
+public class Kata
+{
+    public static char GetGrade(int s1, int s2, int s3)
+    {
+        int score = (s1 + s2 + s3) / 3;
+
+        if (score >= 90 && score <= 100)
+        {
+            return 'A';
+        }
+        else if (score >= 80 && score < 90)
+        {
+            return 'B';
+        }
+        else if (score >= 70 && score < 80)
+        {
+            return 'C';
+        }
+        else if (score >= 60 && score < 70)
+        {
+            return 'D';
+        }
+        else
+        {
+            
+            return 'F';
+        }
+    }
+}
+
 
 */
