f6f87eb42048c01e626387c904e5eb4adad75b79
rencdr <demirerencan@outlook.com>
2024-01-16 18:23:25 +0300

    problem 8 added

--- csharp_problems.cs
+++ csharp_problems.cs
@@ -115,3 +115,20 @@ public class Kata
   }
 }
 */
+/*
+8.Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. 
+You can ignore any need to sanitize words or add punctuation, 
+but you should add spaces between each word. 
+Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+public class Kata
+{
+    public static string Smash(string[] words)
+    {
+        string result = string.Join(" ", words);
+        result = result.Trim();
+        return result;
+    }
+}
+
+*/
