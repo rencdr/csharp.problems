3391a192aa184ce4e2a539058d9147856db89120
rencdr <demirerencan@outlook.com>
2024-01-17 20:23:45 +0300

    problem 11 added

--- csharp_problems.cs
+++ csharp_problems.cs
@@ -193,4 +193,29 @@ public class Kata
     }
 }
 */
+/*
+11.In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
 
+using System.Collections;
+using System.Collections.Generic;
+
+public class ListFilterer
+{
+   public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+    {
+        List<int> integers = new List<int>();
+
+        foreach (var item in listOfItems)
+        {
+            if (item is int)
+            {
+                integers.Add((int)item);
+            }
+        }
+
+        return integers;
+    }
+}
+
+
+*/
