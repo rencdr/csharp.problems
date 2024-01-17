606db4eb1f9d0e526e30713fbf922eac20e7ebd0
rencdr <demirerencan@outlook.com>
2024-01-17 20:28:28 +0300

    problem 12 added

--- csharp_problems.cs
+++ csharp_problems.cs
@@ -216,6 +216,19 @@ public class ListFilterer
         return integers;
     }
 }
+*/
+/*
+12.Write a function named setAlarm/set_alarm/set-alarm/setalarm (depending on language) which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise
 
-
+public class Kata {
+  public static bool SetAlarm(bool employed, bool vacation) {
+    if (employed == true && vacation== false){
+      return true;
+      
+    }else{
+      return false;
+    }
+  }
+}
 */
