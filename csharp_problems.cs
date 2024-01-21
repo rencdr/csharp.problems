ae95017b751dbb0026cb5736dba22e034925a609
rencdr <demirerencan@outlook.com>
2024-01-21 12:28:08 +0300

    problem 20 added

--- csharp_problems.cs
+++ csharp_problems.cs
@@ -391,3 +391,22 @@ public class Kata
   }
 }
 */
+/*
+20.Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+using System.Collections.Generic;
+
+public class LineNumbering 
+{
+    public static List<string> Number(List<string> lines) 
+    {
+        for (int i = 0; i < lines.Count; i++)
+        {
+            lines[i] = $"{i + 1}: {lines[i]}";
+        }
+        return lines;
+    }
+}
+*/
